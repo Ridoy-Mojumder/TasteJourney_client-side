788225555e29e7498b69dcd885adbd7e54bd6fe5
Ridoy-Mojumder <hridoymoju463@gmail.com>
2024-05-15 04:20:12 +0600

    modify error page

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,8 @@
-
 import Lottie from 'react-lottie';
 import animationData from './404error.json';
 import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
-
     const defaultOptions = {
         loop: true,
         autoplay: true,
@@ -16,18 +14,18 @@ const ErrorPage = () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-4">
-            <div className="mb-6 md:mb-0">
+            <div className="mb-12">
                 <Lottie options={defaultOptions} className="h-auto w-auto" />
             </div>
-            <h1 className="text-3xl font-bold text-gray-800">This is an error page</h1>
-            <p className="text-lg text-gray-600">Sorry, the page you are looking for cannot be found.</p>
-            <Link to='/'> 
-                <button
-                    className="mt-4 px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 transition duration-200"
-                >
-                    Go Home
-                </button>
-            </Link>
+            <div className="text-center">
+                <h1 className="text-4xl font-bold text-gray-800 mb-4">Oops! Page Not Found</h1>
+                <p className="text-lg text-gray-600 mb-8">The page you are looking for might have been removed or its name changed.</p>
+                <Link to="/">
+                    <button className="px-8 py-3 text-lg text-white bg-blue-500 rounded-md hover:bg-blue-700 transition duration-200">
+                        Go Home
+                    </button>
+                </Link>
+            </div>
         </div>
     );
 };
